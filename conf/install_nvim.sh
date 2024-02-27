beca5a500d833cd327d3b91555d869ef280650c5
berghetti <40703575+berghetti@users.noreply.github.com>
2024-02-26 21:49:26 -0400

    Create install_nvim.sh

---
+++ conf/install_nvim.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+install_nodejs()
+{
+  curl -sL install-node.vercel.app/lts | sudo bash
+}
+
+install_requeriments()
+{
+  sudo apt install -y git xz-utils ripgrep
+  install_nodejs
+}
+
+install_neovim()
+{
+  if [ -x /opt/nvim-linux64/bin/nvim ]; then
+    return
+  fi
+
+  curl -LOs https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+  sudo rm -rf /opt/nvim
+  sudo tar -C /opt -xzf nvim-linux64.tar.gz
+  sudo rm nvim-linux64.tar.gz
+
+  echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+  echo "alias v='nvim'" >> ~/.bashrc
+  . ~/.bashrc
+}
+
+install_plug()
+{
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  /opt/nvim-linux64/bin/nvim -es -c "PlugInstall" -c "qa"
+  /opt/nvim-linux64/bin/nvim -es -c "CocInstall" -c "qa"
+}
+
+install_conf()
+{
+  curl -LOs https://raw.githubusercontent.com/berghetti/testes/master/conf/.vimrc
+  mv .vimrc ~/.vimrc
+  mkdir -p ~/.config/nvim/
+  echo -e 'set runtimepath^=~/.vim runtimepath+=~/.vim/after\n'\
+	  'let &packpath = &runtimepath\n'\
+	  'source ~/.vimrc' > ~/.config/nvim/init.vim
+
+}
+
+install_requeriments
+install_neovim
+install_conf
+install_plug
