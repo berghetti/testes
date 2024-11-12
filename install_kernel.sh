efe1a42492b25ada921f89cabde9801931e20b0f
berghetti <40703575+berghetti@users.noreply.github.com>
2024-11-12 11:29:44 -0400

    Update install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -5,6 +5,7 @@ mkdir ktemp
 pushd ktemp
 
 wget https://kernel.ubuntu.com/mainline/v4.4.185/linux-headers-4.4.185-0404185_4.4.185-0404185.201907100448_all.deb \
+  https://kernel.ubuntu.com/mainline/v4.4.185/linux-headers-4.4.185-0404185-generic_4.4.185-0404185.201907100448_amd64.deb
   https://kernel.ubuntu.com/mainline/v4.4.185/linux-modules-4.4.185-0404185-generic_4.4.185-0404185.201907100448_amd64.deb \
   https://kernel.ubuntu.com/mainline/v4.4.185/linux-image-unsigned-4.4.185-0404185-generic_4.4.185-0404185.201907100448_amd64.deb
 
