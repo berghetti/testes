a1c8a202a4efd75262e069895ea77ed0a80a0544
berghetti <40703575+berghetti@users.noreply.github.com>
2024-12-09 12:17:21 -0400

    Update install_nvim.sh

--- nvim/install_nvim.sh
+++ nvim/install_nvim.sh
@@ -18,7 +18,7 @@ install_neovim()
     return
   fi
 
-  curl -LOs https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+  curl -LOs https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
   #curl -LOs https://github.com/neovim/neovim-releases/releases/download/v0.10.2/nvim-linux64.tar.gz
   sudo rm -rf /opt/nvim
   sudo tar -C /opt -xzf nvim-linux64.tar.gz
