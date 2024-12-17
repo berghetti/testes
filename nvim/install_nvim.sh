240784c67863c5fd3c5bd7562f7434ae6cf52166
berghetti <40703575+berghetti@users.noreply.github.com>
2024-12-16 22:05:58 -0400

    Update install_nvim.sh

--- nvim/install_nvim.sh
+++ nvim/install_nvim.sh
@@ -47,12 +47,10 @@ install_plug()
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' \
   && \
-  /opt/nvim-linux64/bin/nvim -c "PlugInstall" -c "qa" \
+  /opt/nvim-linux64/bin/nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa" \
   && \
-  /opt/nvim-linux64/bin/nvim -c "CocInstall" -c "qa"
+  /opt/nvim-linux64/bin/nvim -es -u init.vim -i NONE -c "CocInstall" -c "qa"
 }
-
-pushd /tmp
 
 install_conf()
 {
@@ -65,12 +63,11 @@ install_conf()
 
 }
 
-popd
-
-
-
+pushd /tmp
 
 install_requeriments
 install_neovim
 install_conf
 install_plug
+
+popd
